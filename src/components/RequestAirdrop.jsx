93d586999a82f27b34a0aa8e4f13317aef18b9cf
Harsh Ninave <harshninave58@gmail.com>
2025-07-19 23:55:26 +0530

    feat: Logic for requesting an airdrop!!

---
+++ src/components/RequestAirdrop.jsx
@@ -0,0 +1,26 @@
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+
+function RequestAirdrop() {
+  const wallet = useWallet();
+  const { connection } = useConnection();
+  async function requestAirdrop() {
+    const amount = document.getElementById('amount').value;
+    if (!amount) {
+      alert('Please enter an amount');
+      return;
+      }
+      
+    await connection.requestAirdrop(wallet.publicKey, amount * 1000000000)
+
+    console.log(`Airdropping ${amount} tokens...`);
+    alert(`Airdropped ${amount} tokens successfully to ${wallet.publicKey.toBase58()}`);
+  }
+  return (
+    <>
+      <input id="amount" type="text" placeholder="Amount" />
+      <button onClick={requestAirdrop}>Request Airdrop</button>
+    </>
+  );
+}
+
+export default RequestAirdrop;
